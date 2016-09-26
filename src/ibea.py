1c4a27cc7b403bf19954c9926d5e9d8c40567d42
Aris Tritas <a.tritas@gmail.com>
2016-09-26 16:04:17 +0200

    Environmental selection

--- src/ibea.py
+++ src/ibea.py
@@ -5,16 +5,21 @@ import numpy as np
 class IBEA(object):
     def __init__(self,
                  rho=1.1,
-                 kappa=0.05, # fitness scaling ratio
-                 alpha=10, # population size
-                 mu=1, # number of individuals selected as parents
-                 offspring=1, # number of offspring individuals
+                 kappa=0.05, 
+                 alpha=10, 
+                 mu=1, 
+                 offspring=1, 
                  seed=1):
+        # --- Algorithm parameters
         self.rho = rho
-        self.kappa = kappa
-        self.alpha = alpha
-        self.mu = mu
-        self.offspring = offspring
+        self.kappa = kappa         # fitness scaling ratio
+        self.alpha = alpha         # population size
+        self.mu = mu               # number of individuals selected as parents
+        self.offspring = offspring # number of offspring individuals
+        # --- Population data structures
+        self.pop_vectors = None
+        self.population = []
+        # --- Random state
         np.random.seed(seed)
 
     def __str__(self):
@@ -22,31 +27,46 @@ class IBEA(object):
             .format()
 
     def ibea(self, fun, lbounds, ubounds, budget):
-        lbounds, ubounds = np.array(lbounds), np.array(ubounds)
-        dim, x_min, f_min = len(lbounds), (lbounds + ubounds) / 2, None
-        done = False
-        # 1. Initialization
-        self.population = np.zeros((self.alpha, dim), dtype=np.float64)
+        self.lbounds, self.ubounds = np.array(lbounds), np.array(ubounds)
+        dim, f_min = len(lbounds), None
+
+        # 1. Initial population of size alpha
+        # Sampled from the uniform distribution [lbounds, ubounds]
+        self.population = list(range(self.alpha))
+        self.pop_vectors = np.random.rand(self.alpha, dim) \
+                           * (ubounds - lbounds) + lbounds
+        # Rescaling objective values to [0,1]
+        self.objective_values = self.rescale(np.array([fun(x) for x in self.pop_vectors]))
+        assert self.objective_values.shape == (self.alpha, dim), 'Not the right dimension'
         self.fitness_values = np.zeros(self.alpha, dtype=np.float64)        
+        done = False
         generation = 0 
         while not done:
             # 2. Fitness assignment
             for i in range(self.alpha):
                 self.fitness_values[i] = self.compute_fitness(i)
-            # 3.1 Environmental selection
-            unfit_individual = self.fitness_values.argmin()
-            # 3.2 Remove individual (temporary hack)
-            self.population[unfit_individual, :] = np.infty
-            # 3.3 Update fitness values
-            for i in range(self.alpha):
-                self.fitness_values[i] += np.exp(-self.indicator_e(unfit_individual,i)/self.kappa)
+            # 3 Environmental selection
+            env_selection_ok = False
+            while not env_selection_ok:
+                # 3.1 Environmental selection
+                unfit_individual = self.fitness_values.argmin()
+                # 3.2 Remove individual
+                self.population.remove(unfit_individual)
+                # 3.3 Update fitness values
+                for i in self.population:
+                    self.fitness_values[i] += np.exp(
+                        -self.indicator_e(unfit_individual,i)/self.kappa)
+                env_selection_ok = len(self.population) == self.alpha
 
             # 4. Check convergence condition
             done = generation >= budget
             if done: break
             # 5. Mating selection
-            
+            # Perform binary tournament selection with replacement on P in order
+            # to fill the temporary mating pool P'.
             # 6. Variation
+            # Apply recombination and mutation operators to the mating pool P' and add
+            # the resulting offspring to P
             generation += 1
 
         return f_min
@@ -54,7 +74,7 @@ class IBEA(object):
     def compute_fitness(self, ind):
         ''' For all vectors in P\{ind}, compute pairwise indicator function
         and sum to get fitness value.'''
-        for i in range(self.alpha):
+        for i in self.population:
             if i != ind:
                 exp_indic = np.exp(-self.indicator_e(i, ind)/self.kappa)
                 self.fitness_values[ind] -= exp_indic # -= instead of += because of negative sum
@@ -69,10 +89,8 @@ class IBEA(object):
         return (f_x - lb)/(ub - lb)
 
     def indicator_e(self, i1, i2):
-        x1 = self.population[i1]
-        x2 = self.population[i2]        
-        df = x1 - x2
-        return max(0, df.min())
+        df = self.objective_values[i1] - self.objective_values[i2]
+        return df.min()
 
 if __name__ == '__main__':
     """call `experiment.main()`"""
